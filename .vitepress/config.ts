4fbc73c51106032eed764d7d7b4544a75be06cb3
niuee <vntchang@gmail.com>
2025-12-11 18:46:54 +0800

    fix: set base

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   srcDir: "docs",
+  base: 'ue-too.github.io/documentation/',
   
   title: "uē-tôo",
   description: "Everything about uē-tôo",
